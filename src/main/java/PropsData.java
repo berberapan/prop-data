1f01df91a3cdfbe44e9f7415891a4357238a504c
Josef <josef@boukdir.se>
2023-12-07 11:56:59 +0100

    Search class added, test data for team matches

--- src/main/java/PropsData.java
+++ src/main/java/PropsData.java
@@ -2,5 +2,8 @@ public class PropsData {
 
     public static void main(String[] args) {
 
+        var search = new Search();
+
+        System.out.println(search.getSearchedFromDate());
     }
 }
---
+++ src/main/java/Search.java
@@ -0,0 +1,46 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Search {
+    private final String searchedTeamID;
+    private long searchedFromDate;
+
+
+    public Search() {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("""
+                Put in the Team ID for the team you want to see stats for. E.g. 2163 for Team Liquid
+                Just put in the numbers and press Enter
+                """);
+        this.searchedTeamID = scanner.nextLine();
+
+        System.out.println("""
+                Put in from what date you want to search FROM. Please add in following format DD/MM/YYYY
+                Note that the further back you go the more time the search will take as API calls are restricted.
+                """);
+        convertDateToUnix(scanner.nextLine());
+    }
+
+    private void convertDateToUnix(String input) {
+        var date = input.split("/");
+        try {
+            var calendar = Calendar.getInstance();
+            calendar.set(Integer.parseInt(date[2]), Integer.parseInt(date[1])-1, Integer.parseInt(date[0]),
+                    0,0,0);
+            Date inputDate = calendar.getTime();
+            this.searchedFromDate = inputDate.toInstant().getEpochSecond();
+        } catch (Exception e) {
+            System.out.println("Date given in wrong format. Try again.");
+        }
+    }
+
+    public String getSearchedTeamID() {
+        return searchedTeamID;
+    }
+
+    public long getSearchedFromDate() {
+        return searchedFromDate;
+    }
+}
